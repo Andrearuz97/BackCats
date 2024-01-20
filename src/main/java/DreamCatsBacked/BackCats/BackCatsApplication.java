e8546c266f11e3a6c1edf24d2bdf6284a661cdd9
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2024-01-20 13:39:27 +0100

    FIRST

---
+++ src/main/java/DreamCatsBacked/BackCats/BackCatsApplication.java
@@ -0,0 +1,13 @@
+package DreamCatsBacked.BackCats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DreamCatsBacked/BackCats/BackCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package DreamCatsBacked.BackCats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
